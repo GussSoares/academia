3393b9a05da0b28113944d1bf12abfbf707050da
gustavo <gustavo.soares.cdc@gmail.com>
2019-12-06 23:45:56 -0300

    configurando

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nr4!^hy_^_059rm66#3*c%h0wsi6-_&+xu_vyxm4a&1r-rlrr!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['academia-system.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
